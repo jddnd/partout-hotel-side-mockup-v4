52f1609cf9fe6fd97f8b8598d41bd36d2fc85cdf
jddnd <68731871+jddnd@users.noreply.github.com>
2026-09-11 23:53:39 +0200

    fix(hotel): keep campaigns route stateless

--- src/hotel/campaigns/campaigns-page.tsx
+++ src/hotel/campaigns/campaigns-page.tsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
 import { campaigns } from '../../data/mock/campaigns'
-import { CampaignCreateDrawer } from './campaign-create-drawer'
 import { CampaignsHeader } from './campaigns-header'
 import { CampaignsTable } from './campaigns-table'
 import { CampaignsTabs } from './campaigns-tabs'
 
 export function CampaignsPage() {
-  const [createOpen, setCreateOpen] = useState(false)
-
   return (
     <div>
-      <CampaignsHeader onCreate={() => setCreateOpen(true)} />
+      <CampaignsHeader />
       <CampaignsTabs />
       <CampaignsTable campaigns={campaigns} />
-      {createOpen ? <CampaignCreateDrawer onClose={() => setCreateOpen(false)} /> : null}
     </div>
   )
 }
