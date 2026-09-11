a8c2f59d60826939cd75cac9700ab20ad6ce6d2e
jddnd <68731871+jddnd@users.noreply.github.com>
2026-09-11 23:50:12 +0200

    feat(hotel): open campaign creation flow from campaigns

--- src/hotel/campaigns/campaigns-page.tsx
+++ src/hotel/campaigns/campaigns-page.tsx
@@ -1,14 +1,19 @@
+import { useState } from 'react'
 import { campaigns } from '../../data/mock/campaigns'
+import { CampaignCreateDrawer } from './campaign-create-drawer'
 import { CampaignsHeader } from './campaigns-header'
 import { CampaignsTable } from './campaigns-table'
 import { CampaignsTabs } from './campaigns-tabs'
 
 export function CampaignsPage() {
+  const [createOpen, setCreateOpen] = useState(false)
+
   return (
     <div>
-      <CampaignsHeader />
+      <CampaignsHeader onCreate={() => setCreateOpen(true)} />
       <CampaignsTabs />
       <CampaignsTable campaigns={campaigns} />
+      {createOpen ? <CampaignCreateDrawer onClose={() => setCreateOpen(false)} /> : null}
     </div>
   )
 }
