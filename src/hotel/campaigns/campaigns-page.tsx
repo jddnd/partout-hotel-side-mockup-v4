9b7039a05e31e9c8211c210b961a50c95da0dee9
jddnd <68731871+jddnd@users.noreply.github.com>
2026-09-12 00:11:31 +0200

    feat(hotel): show locally created campaigns

--- src/hotel/campaigns/campaigns-page.tsx
+++ src/hotel/campaigns/campaigns-page.tsx
@@ -1,14 +1,17 @@
 import { campaigns } from '../../data/mock/campaigns'
+import { getMockCampaigns } from './campaign-mock-storage'
 import { CampaignsHeader } from './campaigns-header'
 import { CampaignsTable } from './campaigns-table'
 import { CampaignsTabs } from './campaigns-tabs'
 
 export function CampaignsPage() {
+  const visibleCampaigns = getMockCampaigns(campaigns)
+
   return (
     <div>
       <CampaignsHeader />
       <CampaignsTabs />
-      <CampaignsTable campaigns={campaigns} />
+      <CampaignsTable campaigns={visibleCampaigns} />
     </div>
   )
 }
