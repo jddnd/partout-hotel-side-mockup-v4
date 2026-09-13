f8a98e113dee9c1e4015140b35d7494eee618bf5
jddnd <68731871+jddnd@users.noreply.github.com>
2026-09-13 23:34:06 +0200

    feat(hotel): create collaboration from accepted invitation

--- src/entities/collaboration/collaboration.ts
+++ src/entities/collaboration/collaboration.ts
@@ -19,3 +19,23 @@ export function collaborationFromAcceptedApplication({
     sourceApplicationId: applicationId,
   }
 }
+
+export function collaborationFromAcceptedInvitation({
+  invitationId,
+  creatorId,
+  campaignId,
+  relationshipId,
+}: Readonly<{
+  invitationId: string
+  creatorId: string
+  campaignId: string
+  relationshipId: string
+}>): HotelCollaboration {
+  return {
+    id: `collaboration-${invitationId}`,
+    creatorId,
+    relationshipId,
+    campaignId,
+    sourceCampaignInvitationId: invitationId,
+  }
+}
